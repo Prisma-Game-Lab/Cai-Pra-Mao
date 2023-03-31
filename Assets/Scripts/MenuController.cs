813a3516cd8e6e83df4d1f9dc85b806eb784251c
vthork <56010939+vthork@users.noreply.github.com>
2023-03-31 15:36:28 -0300

    Cena controle

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,30 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void PlayGame ()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Gera um indice para as cenas que estao no buiding settings
     }
-    public void QuitGame ()
+    public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Saindo");
     }
-    public void GoConfigurations ()
+    public void GoConfigurations()
     {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);  
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
-    public void GoCredits ()
+    public void GoCredits()
     {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);  
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
     }
-    public void GoBackMenu ()
+    public void GoBackMenu()
     {
-      SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Menu");
     }
-    public void StartFight ()
+    public void StartFight()
     {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 7);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 7);
+    }
+
+    public void GoController()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 6); // por algum motivo o +6 me levou pra cena de indice 8
     }
 
 }
