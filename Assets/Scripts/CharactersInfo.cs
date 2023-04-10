9b99c384bcd65ae383deba6546e364d69ce1733d
MadudaJellyfish <105326941+MadudaJellyfish@users.noreply.github.com>
2023-04-10 19:52:36 -0300

    Seleção de personagens (quase completo)

---
+++ Assets/Scripts/CharactersInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharInfo : MonoBehaviour
+{
+    public int posX;
+    public int posY;
+    public string characterId;
+}
+
